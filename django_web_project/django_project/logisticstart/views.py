2cc8ffee675dd8bf64c16b7b533965aad28a371d
buayayee <sh0wmer3y@gmail.com>
2024-05-21 21:22:03 +0800

    Revert "Login is up"
    This reverts commit 2a32bae2117e362f57eef5362fd9c977c000dc70.

--- django_web_project/django_project/logisticstart/views.py
+++ django_web_project/django_project/logisticstart/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render,redirect 
-from django.contrib.auth import authenticate, login, logout
-from .forms import SignupForm, LoginForm
+from django.shortcuts import render
 
 # Create your views here.
 def logistichome(request):
@@ -9,41 +7,12 @@ def logistichome(request):
 def logisticform(request):
     return render(request, 'logisticstart/form.html') #return logisticstart/templates/logisticstart/form.html
 
-# def logisticlogin(request):
-#     return render(request, 'logisticstart/login.html') #return logisticstart/templates/logisticstart/login.html
+def logisticlogin(request):
+    return render(request, 'logisticstart/login.html') #return logisticstart/templates/logisticstart/login.html
 
 def logisticitems_list(request):
     return render(request, 'logisticstart/items_list.html') #return logisticstart/templates/logisticstart/items_list.html
 
-# signup page
-def logisticsignup(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('logisticstart-login')
-    else:
-        form = SignupForm()
-    return render(request, 'logisticstart/signup.html', {'form': form})
-
-# login page
-def logisticlogin(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user:
-                login(request, user)    
-                return redirect('logisticstart-test')
-    else:
-        form = LoginForm()
-    return render(request, 'logisticstart/login.html', {'form': form})
-
-def logistictest(request):
-    return render(request, 'logisticstart/test.html')
-    
 def custom_page_not_found_view(request, exception):
     return render(request, "errors/404.html", {})
 
