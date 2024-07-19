8a08c277e62728d69d9b368ca79b5a6d42534cf6
BoonYeew <147385740+BoonYeew@users.noreply.github.com>
2024-07-19 18:12:44 +0800

    Delete django_web_project/django_project/nginx_waitress directory

--- django_web_project/django_project/nginx_waitress/runserver.py
+++
@@ -1,8 +0,0 @@
-from waitress import serve
-
-#AZURE
-from django_project.wsgi import application
-# documentation: https://docs.pylonsproject.org/projects/waitress/en/stable/api.html
-
-if __name__ == '__main__':
-    serve(application, host = 'localhost', port='8080')
