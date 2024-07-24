7c3a5a76c800a505626bc89474f2abc8b068a722
prezgamer <50046059+prezgamer@users.noreply.github.com>
2024-07-24 14:47:44 +0800

    Updated company_phone number and change avatar

--- django_web_project/django_project/logisticstart/models.py
+++ django_web_project/django_project/logisticstart/models.py
@@ -10,7 +10,7 @@ class Accounts(models.Model):
     password = models.CharField(max_length=100)
     company_name = models.CharField(max_length=100)
     company_address = models.CharField(max_length=255)    
-    company_phonenumber = models.CharField(max_length=15)
+    company_phonenumber = models.IntegerField(max_length=15)
     
 
 class NewWarehouseListing(models.Model):
