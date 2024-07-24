b71644d973fcc500ae4d384841cfddbab3717bd5
BoonYeew <147385740+BoonYeew@users.noreply.github.com>
2024-07-24 20:36:22 +0800

    Update settings.py

--- django_web_project/django_project/django_project/settings.py
+++ django_web_project/django_project/django_project/settings.py
@@ -32,7 +32,7 @@ DEBUG = False
 #ALLOWED_HOSTS = []
 
 #AZURE
-ALLOWED_HOSTS = ['localhost','windows-nginx','40.81.240.133']
+ALLOWED_HOSTS = ['localhost','windows-nginx','40.81.240.133',' server03.centralindia.cloudapp.azure.com']
 
 
 # Application definition (do not touch this)
@@ -127,14 +127,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 #AZURE
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'logisticstart/static')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'logisticstart/static')
+#]
 
 MEDIA_URL = '/media/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
